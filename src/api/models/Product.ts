be575eebf6afb44f7f3e485be02733a99f8c3f45
Ibrahim <ibrahimmk212@gmail.com>
2025-06-08 12:57:49 +0100

    added vendor category endponts

--- src/api/models/Product.ts
+++ src/api/models/Product.ts
@@ -7,7 +7,6 @@ export interface Product extends Document {
     description: string;
     vendor: mongoose.Types.ObjectId;
     category: mongoose.Types.ObjectId;
-    marketCategory: mongoose.Types.ObjectId;
     subcategory: mongoose.Types.ObjectId;
     ratings: number;
     images: string;
@@ -25,11 +24,6 @@ const productSchema = new Schema<Product>(
         category: {
             type: Schema.Types.ObjectId,
             ref: 'Category',
-            required: true
-        },
-        marketCategory: {
-            type: Schema.Types.ObjectId,
-            ref: 'MarketCategory',
             required: true
         },
         subcategory: {
