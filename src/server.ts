d9049b326043d06d24a1599604e6e6d1bb7be955
Ibrahim <ibrahimmk212@gmail.com>
2025-09-08 18:20:12 +0100

    fixes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Response, Request } from 'express';
+import express, { Response, Request } from 'express';
 import compression from 'compression';
 import cors from 'cors';
 import * as swaggerUi from 'swagger-ui-express';
@@ -8,38 +8,31 @@ import { Application } from 'express';
 import AppConfig from './config/appConfig';
 import { specs } from './utils/swagger';
 import errorHandler from './api/middlewares/handlers/error';
-import expressfileupload from 'express-fileupload';
 
 export function createServer(): Application {
     const app = express();
 
-    const corsOptions = {
-        origin: (
-            origin: string | undefined,
-            callback: (err: Error | null, allow?: boolean) => void
-        ) => {
-            console.log('CORS check for origin:', origin);
-        },
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    };
-
-    // apply CORS for all requests
-    app.use(cors(corsOptions));
-    // make sure preflight OPTIONS requests are handled too
-    app.options('*', cors(corsOptions));
+    // ---- ALLOW ALL CORS ----
+    app.use(
+        cors({
+            origin: '*',
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization']
+        })
+    );
+    app.options('*', cors()); // preflight
+    // -------------------------
 
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(compression());
     app.use(MorganMiddleware);
-    // app.use(expressfileupload());
     app.use(`/api/${AppConfig.app.apiVersion}`, routesV1);
 
     app.get('/', (req: Request, res: Response) => {
         return res.json({ hello: 'hy' });
     });
+
     if (AppConfig.app.isDevelopment) {
         app.use(
             `/docs/${AppConfig.app.apiVersion}`,
@@ -49,18 +42,16 @@ export function createServer(): Application {
     }
 
     app.use('/monnify', (req: Request, res: Response) => {
-        console.log(req.params);
-        // const error = Error('Page Not FOund');
         return res.status(404).json({ success: false, message: 'MOnify' });
     });
 
     app.use(errorHandler);
-    // Invalid Endpoint
+
     app.use((req: Request, res: Response) => {
-        // const error = Error('Page Not FOund');
         return res
             .status(404)
             .json({ success: false, message: 'Page Not Found' });
     });
+
     return app;
 }
