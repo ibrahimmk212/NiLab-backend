c3f7580209c7775a3c49f0c0132207fde78eb6ce
Ibrahim <ibrahimmk212@gmail.com>
2025-09-11 08:11:02 +0100

    fixes

--- src/api/routes/v1/public/marketCategories.ts
+++ src/api/routes/v1/public/marketCategories.ts
@@ -1,12 +1,9 @@
-
 import { Router } from 'express';
-import { Validate } from '../../../middlewares/validator';
 import MarketCategoryController from '../../../controllers/vendors/MarketCategoryController';
 
-
 const marketCategoryRouter: Router = Router();
-marketCategoryRouter.get('/', MarketCategoryController.getAll)
-marketCategoryRouter.get('/:id', MarketCategoryController.getSingle)
+marketCategoryRouter.get('/', MarketCategoryController.getAll);
+marketCategoryRouter.get('/:id', MarketCategoryController.getSingle);
 // marketCategoryRouter.put('/:id', MarketCategoryController)
 
 export default marketCategoryRouter;
